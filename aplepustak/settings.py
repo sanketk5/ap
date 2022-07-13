ea6087f2737b4623d589ffc6916afd7c3fcf4e4c
Sanket pachpute <pachputesanket1@gmail.com>
2022-07-13 07:37:52 +0530

    true

--- aplepustak/settings.py
+++ aplepustak/settings.py
@@ -135,7 +135,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_PRELOAD = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 

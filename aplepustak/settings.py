37edac1dbf404714fc2f8954f02a29b0b2e4289d
Sanket pachpute <pachputesanket1@gmail.com>
2022-07-14 18:22:35 +0530

    static

--- aplepustak/settings.py
+++ aplepustak/settings.py
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/home'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

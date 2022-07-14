56b60ab4a9beaf3e4c9e426e6795133d71939fe1
Sanket pachpute <pachputesanket1@gmail.com>
2022-07-15 00:58:05 +0530

    static

--- aplepustak/settings.py
+++ aplepustak/settings.py
@@ -153,8 +153,7 @@ LOGOUT_REDIRECT_URL = '/home'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'

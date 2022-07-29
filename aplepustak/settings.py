7a8ae848dc7425131486069a02dddb3cdfaa40ce
Sanket pachpute <pachputesanket1@gmail.com>
2022-07-29 17:40:58 +0530

    host change

--- aplepustak/settings.py
+++ aplepustak/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://139.59.75.15', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['139.59.75.15', 'localhost', '127.0.0.1']
 
 # Application definition
 
